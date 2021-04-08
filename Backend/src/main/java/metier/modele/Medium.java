729c209f327f73d2de8d795b93306eac5c17294d
rgalle <romain.galle@insa-lyon.fr>
2021-04-08 10:33:04 +0200

    Quickfix Medium

--- Backend/src/main/java/metier/modele/Medium.java
+++ Backend/src/main/java/metier/modele/Medium.java
@@ -68,9 +68,4 @@ public class Medium {
     public Long getId() {
         return id;
     }
-
-    @Override
-    public String toString() {
-        return "Medium{" + "id=" + id + ", genre=" + genre + ", denomination=" + denomination + ", presentation=" + presentation + '}';
-    }
 }
