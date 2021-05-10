956f8ebe583b6e5dd0a026bdbfdbe3fd4503c869
rgalle <romain.galle@insa-lyon.fr>
2021-05-10 16:02:22 +0200

    Inscription WIP, ajout du Jpa init dans ActionServlet

--- Frontend/src/main/java/fr/projetdasi/frontend/serialisations/SerialisationInscription.java
+++ Frontend/src/main/java/fr/projetdasi/frontend/serialisations/SerialisationInscription.java
@@ -5,13 +5,19 @@
  */
 package fr.projetdasi.frontend.serialisations;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SerialisationInscription extends Serialisation {
 
     @Override
-    public void serialiser(HttpServletRequest request, HttpServletResponse response) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException 
+    {
+        boolean success = (boolean) request.getAttribute("success");
+        if(success)
+        {
+            Client client = (Client) request.getAttribute("client");
+        }
     }
 }
