bb803f6dc3cbf19928dc2e44298c72421885c35b
onyr <rascoussier.florian@gmail.com>
2021-05-10 16:21:15 +0200

    Commit before merge

--- Frontend/src/main/webapp/js/front_page.js
+++ Frontend/src/main/webapp/js/front_page.js
@@ -6,15 +6,19 @@ function pageInit() {
 
 $(document).ready(pageInit());
 
-
 async function voirMediums() {
     console.log("[debug] clic sur 'Voir nos médiums'");
     try {
         const res = await $.ajax({
-            url: './ActionServlet?action=get_mediums',
+            url: './ActionServlet?action=',
+            data: {
+                todo:'lister-mediums'
+            },
             method: 'GET',
             dataType: 'json'
         })
+        console.log(res);
+
     } catch (err) {
         console.log(err);
     }
