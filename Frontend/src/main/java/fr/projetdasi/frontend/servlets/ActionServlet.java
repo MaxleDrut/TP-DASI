e8539eee9b3f39bfd026b72ec14d47cba8eaff7f
rgalle <romain.galle@insa-lyon.fr>
2021-05-10 14:19:14 +0200

    Action servlet correction

--- Frontend/src/main/java/fr/projetdasi/frontend/servlets/ActionServlet.java
+++ Frontend/src/main/java/fr/projetdasi/frontend/servlets/ActionServlet.java
@@ -29,18 +29,15 @@ public class ActionServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ActionServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ActionServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        String action = request.getParameter("action");
+        
+        switch(action)
+        {
+            
+            default:
+                // retourner page d'erreur
+                break;
         }
     }
 
